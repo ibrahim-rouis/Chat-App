816f5be75f69eb58ab5cc4fa7bcf1f2b15b01986
Ibrahim Rouis <ibrahimrouisdev@proton.me>
2025-06-30 16:49:12 +0100

    Setup firebase project and emulators

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,12 +9,16 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   if (kDebugMode) {
     await Firebase.initializeApp(demoProjectId: 'demo-dev');
-  } else {
-    await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
+    try {
+      FirebaseFirestore.instance.useFirestoreEmulator('127.0.0.1', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('127.0.0.1', 9099);
+    } catch (e) {
+      debugPrint('Failed to connect to emulators, reason: $e');
+      exit(1);
+    }
   }
   runApp(const MyApp());
 }
@@ -18,7 +26,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,6 +43,28 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Hello, World!')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Hello, World!'),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance
+                    .collection('test')
+                    .add({'message': 'FlutterFire Firestore working'})
+                    .then(
+                      (docRef) =>
+                          debugPrint('Firestore add document successful'),
+                    )
+                    .catchError((e) => debugPrint(e));
+              },
+              child: const Text('Test Firestore'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
