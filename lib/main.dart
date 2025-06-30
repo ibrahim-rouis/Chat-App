5d27e604bcf115650f8e7c7cd40da1a8f6e39e1a
Ibrahim Rouis <ibrahimrouisdev@proton.me>
2025-06-30 15:19:27 +0100

    Connect app to Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kDebugMode) {
+    await Firebase.initializeApp(demoProjectId: 'demo-dev');
+  } else {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
   runApp(const MyApp());
 }
 
